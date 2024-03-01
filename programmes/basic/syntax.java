76911904ed5dccd25ea963d795d50fee073594c3
AshishDev28 <patelashish0688@gmail.com>
2024-03-01 22:27:08 +0530

    syntax of output

---
+++ programmes/basic/syntax.java
@@ -0,0 +1,9 @@
+package programmes.basic;
+
+public class syntax {
+    public static void main(String[] args){
+        System.out.println("hey i am ashish ");
+        System.out.println("this is my first programme on java");
+    }
+    
+}
