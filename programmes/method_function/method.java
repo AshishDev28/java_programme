1c818e376c749fa2957c3ea03de14a0d1b26cba1
AshishDev28 <patelashish0688@gmail.com>
2024-03-05 00:41:24 +0530

    Create method.java

---
+++ programmes/method_function/method.java
@@ -0,0 +1,14 @@
+package programmes.method_function;
+
+public class method {
+    static void myMethod(){     //function declared
+        System.out.println("I am Ashish kumar patel .");
+
+    }
+
+public static void main(String[] args){
+    myMethod();     //function called 
+    myMethod();     // multiple function is aslo called 
+}
+    
+}
