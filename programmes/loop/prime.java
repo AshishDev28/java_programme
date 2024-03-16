19611967f49ec14bc89d03581934ab480d6b9ba2
AshishDev28 <patelashish0688@gmail.com>
2024-03-17 02:27:12 +0530

    extra topics

---
+++ programmes/loop/prime.java
@@ -0,0 +1,21 @@
+package loop;
+import java.util.*;
+public class prime {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = 0;
+        for(int i =1;i<=n;i++)
+        {
+            if (n%i==0){
+                c++;
+            }
+        }
+        if(c==2){
+            System.out.println(n+" is prime number");
+        }
+        else{
+            System.out.println("not a prime number");
+        }
+    }
+}
